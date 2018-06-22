70d13ae0ba555d13456940138f5e3fb4ede6726a
Gabo C <camacho@diskingressos.com.br>
2018-06-22 18:28:50 -0300

    % modificando modelo

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/agent')
+const setupAdminModel = require('./models/admin')
+const setupClientModel = require('./models/client')
+const setupSerieModel = require('./models/serie')
 const setupMetricModel = require('./models/metric')
 
 module.export = async function (config) {
   const sequelize = setupDatabase(config)
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
+  const ClientModel = setupClientModel(config)
+  const AdminModel = setupAdminModel(config)
+  const SerieModel = setupSerieModel(config)
+
+  AdminModel.hasMany(AgentModel)
+  ClientModel
 
   AgentModel.hasMany(MetricModel)
   MetricModel.belongsTo(AgentModel)
