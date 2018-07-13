3447dd0d9dee4c8d3c00f1179373a81c496973c2
Gabo C <camacho@diskingressos.com.br>
2018-07-13 10:47:10 -0300

    % inquirer e chalk

--- setup.js
+++ setup.js
@@ -1,15 +1,21 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
-// const inquirer = require('inquire')
-// const chalk = require('chalk')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
-// const prompt = inquirer.createPromptModule()
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
-  // const answer = await prompt({
-//
-  // })
+  const answer = await prompt({
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy your database, are you sure?'
+  })
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
 
   const config = {
     database: process.env.DB_NAME || 'fila',
@@ -28,7 +34,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.log(err.message)
+  console.log(`${chalk.red('[fatal error]')}  ${err.message}`)
   console.log(err.stack)
   process.exit(1)
 }
